9998f25abf58483b28eab3e5744679916a8a266d
Kanekami <vascollopes@gmail.com>
2024-11-11 16:39:54 +0000

    021-2

--- projects/021_livraria_math.py
+++ projects/021_livraria_math.py
@@ -2,7 +2,7 @@ import math
 
 n=int(input("Introduza um numero inteiro."))
 
-n_fabs = math.fabs(n)
+n_fabs = int(math.fabs(n))
 print(f"O valor absoluto do valor intruduzido e {n_fabs}.")
 if n >= 0:
     n_fact = math.factorial(n)

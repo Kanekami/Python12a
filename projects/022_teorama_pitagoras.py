7bf621bf18c7c2aa3185a84d422390896ff8812a
Kanekami <vascollopes@gmail.com>
2024-11-11 17:34:50 +0000

    022

---
+++ projects/022_teorama_pitagoras.py
@@ -0,0 +1,9 @@
+import math
+
+print("Introduza os catetos de um triangulo.")
+
+x=int(input("x="))
+y=int(input("y="))
+
+z=math.sqrt(x**2+y**2)
+print(z)
---
+++ projects/023_formula_resolvente.py
@@ -0,0 +1,15 @@
+import math
+
+print("introduza uma equação de segundo grau")
+
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+
+delta = b ** 2 - 4 * a * c
+if delta >= 0:
+    x_1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    x_2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    print(f"x={x_1} ∨ x={x_2}")
+else:
+    print("A equação não tem soluções reais")
